afe66fbf20a26b985503bfe049b0b20d07a5214d
xxysama <374439529@qq.com>
2020-03-25 17:21:35 +0800

    圈子详情-

--- src/foreviews/circledetails/index.vue
+++ src/foreviews/circledetails/index.vue
@@ -12,8 +12,64 @@
                 <el-button size="medium" type="success">加入圈子</el-button>
             </div>
             <div class="circle-board">
-
+                <p>创建于:</p>
+                <div class="circle-intro">
+                        《看电影》杂志现在的阅读者、曾经的阅读者、将来的阅读者......
+                <br>
+                <br>不管怎样，毕竟《看电影》带给我们许多回忆
+                <br>
+                <br>《看电影》3期正刊+1期午夜场，正刊每十天一期。我们将以更为频密、细致的电影内容，回馈影迷、业界的需求。期待您，一如既往的支持。
+                <br>
+                <br>《看电影 午夜场》小组<a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.douban.com%2Fgroup%2Fwuyechang%2F">http://www.douban.co<wbr>m/group/wuyechang/</a>
+                <br>
+                <br>《环球首映》小组<a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.douban.com%2Fgroup%2Fhqsy%2F">http://www.douban.co<wbr>m/group/hqsy/</a>
+                <br>
+                <br>《香港电影》小组<a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.douban.com%2Fgroup%2F194261%2F">http://www.douban.co<wbr>m/group/194261/</a>
+                <br>
+                <br>《看电影》新浪微博 <a href="https://www.douban.com/link2/?url=http%3A%2F%2Ft.sina.com.cn%2Fmovieview" target="_blank" rel="nofollow">http://t.sina.com.cn<wbr>/movieview</a>
+                <br>
+                <br>百度贴吧——看电影杂志吧
+                <br><a href="https://www.douban.com/link2/?url=http%3A%2F%2Ftieba.baidu.com%2Ff%3Fkw%3D%25BF%25B4%25B5%25E7%25D3%25B0%25D4%25D3%25D6%25BE" target="_blank" rel="nofollow">http://tieba.baidu.c<wbr>om/f?kw=%BF%B4%B5%E7<wbr>%D3%B0%D4%D3%D6%BE</a>
+                <br>
+                <br>豆瓣《看电影》杂志档案馆<a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.douban.com%2Fsubject%2F1818673%2F">http://www.douban.co<wbr>m/subject/1818673/</a>
+                <br>
+                <br>时光网《看电影》杂志读者小组<a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.mtime.com%2Fgroup%2Fmovieview%2F&amp;link2key=6c9b7c0099" target="_blank" rel="nofollow">http://www.mtime.com<wbr>/group/movieview/</a>
+                <br>
+                <br>《看电影》出版方大嘴传媒官方论坛 <a href="https://www.douban.com/link2/?url=http%3A%2F%2Fwww.dazui.com&amp;link2key=6c9b7c0099" target="_blank" rel="nofollow">www.dazui.com</a>
+                </div>
             </div>
+
+            <div class="circle-topics">
+              <div class="topic-tab">
+                  <h3>最近讨论</h3>
+              </div>
+              <el-table
+                @row-click = "openTopic"
+                :data="tableData"
+                style="width: 95%">
+                <el-table-column
+                  prop="disscuss"
+                  label="讨论"
+                  width="300">
+                </el-table-column>
+                <el-table-column
+                  prop="name"
+                  label="作者"
+                  width="200">
+                </el-table-column>
+                <el-table-column
+                  prop="reply"
+                  label="回应"
+                  width="">
+                </el-table-column>
+                <el-table-column
+                  prop="date"
+                  label="最后回应"
+                  width="">
+                </el-table-column>
+              </el-table>
+            </div>
+
         </el-main>
         <el-aside width="25%">
             <div class="movie-aside">
@@ -45,14 +101,37 @@
 export default {
   data () {
     return {
-      cid: ''
+      cid: '',
+      tableData: [{
+        disscuss: '测试讨论',
+        date: '2016-05-02',
+        name: '王小虎',
+        reply: '111'
+      }, {
+        disscuss: '测试讨论',
+        date: '2016-05-04',
+        name: '王小虎',
+        reply: '222'
+      }, {
+        disscuss: '测试讨论',
+        date: '2016-05-01',
+        name: '王小虎',
+        reply: '333'
+      }, {
+        disscuss: '测试讨论',
+        date: '2016-05-03',
+        name: '王小虎',
+        reply: '444'
+      }]
     }
   },
   components: {
   },
 
   methods: {
-
+    openTopic (row) {
+      alert(row.reply)
+    }
   },
 
   mounted () {
@@ -105,6 +184,10 @@ export default {
         background: #fff4e8;
         border-radius: 5px;
         position: relative;
-        height: 100px;
+        height: auto;
+    }
+
+    .topic-tab{
+      margin: 30px 0 ;
     }
 </style>
